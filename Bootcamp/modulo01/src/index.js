77ad01020a47957321911035f705c7af2f3c20ed
Miguel Machado <miiguellmp@gmail.com>
2019-02-28 16:09:02 -0300

    SASS

--- Bootcamp/modulo01/src/index.js
+++ Bootcamp/modulo01/src/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
 
 import Button from "./Button";
+import "./style.scss";
 
 class App extends Component {
   state = {
