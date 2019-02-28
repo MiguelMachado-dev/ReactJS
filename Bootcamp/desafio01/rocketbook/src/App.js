066578c696cbb25b3c8e5b63af0591bf909f5f30
Miguel Machado <miiguellmp@gmail.com>
2019-02-28 17:57:57 -0300

    inicio do app

---
+++ Bootcamp/desafio01/rocketbook/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ Bootcamp/desafio01/rocketbook/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ReactBook</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
