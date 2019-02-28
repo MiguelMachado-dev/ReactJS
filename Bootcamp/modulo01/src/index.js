e7ff864ff86242e7c9931d8b5d0653ea317e5087
Miguel Machado <miiguellmp@gmail.com>
2019-02-28 14:30:56 -0300

    DefaultProps & PropsType

--- Bootcamp/modulo01/src/index.js
+++ Bootcamp/modulo01/src/index.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import { render } from "react-dom";
 
 class Button extends Component {
@@ -11,6 +12,15 @@ class Button extends Component {
   }
 }
 
+Button.defaultProps = {
+  children: "Salvar"
+};
+
+Button.PropTypes = {
+  onClick: PropTypes.func.isRequired,
+  childen: PropTypes.string
+};
+
 class App extends Component {
   handleClick() {
     alert("Botão clicado");
@@ -19,6 +29,11 @@ class App extends Component {
     return (
       <Fragment>
         <h1>Hello Rocketseat</h1>
+        <Button
+          onClick={() => {
+            alert("Button 1");
+          }}
+        />
         <Button onClick={this.handleClick}>Enviar</Button>
       </Fragment>
     );
