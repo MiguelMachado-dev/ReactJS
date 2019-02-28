fb359ac100ae3c0651f89fca4517293c24358b02
Miguel Machado <miiguellmp@gmail.com>
2019-02-28 20:29:40 -0300

    app finalizado

--- Bootcamp/desafio01/rocketbook/src/App.js
+++ Bootcamp/desafio01/rocketbook/src/App.js
@@ -10,14 +10,16 @@ class App extends Component {
         name: "Miguel Machado",
         avatar: "https://avatars3.githubusercontent.com/u/29252011?s=460&v=4",
         time: "há 3 minutos",
-        body: "lorem ipsum"
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui libero, venenatis et dui consequat, placerat iaculis justo. Donec nec vestibulum turpis, a vestibulum massa. Cras facilisis odio a sapien euismod maximus. Ut semper ornare ipsum, nec convallis odio interdum vitae. Proin venenatis nunc sed sollicitudin fringilla. Praesent a dapibus est. Nam at lectus eu arcu dapibus eleifend volutpat sed urna. Donec placerat metus sed nunc porta faucibus"
       },
       {
         id: 2,
         name: "Miguel Machado",
         avatar: "https://avatars3.githubusercontent.com/u/29252011?s=460&v=4",
         time: "há 30 minutos",
-        body: "lorem ipsuaksdmkam"
+        body:
+          "Mauris laoreet, orci eu iaculis faucibus, ex purus ornare mauris, sit amet dictum erat neque ut massa. Cras sollicitudin nisi nisl, eu consequat erat auctor eu. Cras semper elit non mauris gravida, quis dignissim mi laoreet. Nam non elit sed dolor euismod ornare et sit amet nulla. Nunc elementum dignissim orci, a bibendum orci pulvinar et. Quisque venenatis enim sit amet vestibulum molestie. Donec condimentum molestie leo feugiat hendrerit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque sagittis erat pretium mi dictum vulputate. Duis mi ipsum, viverra sit amet mollis sit amet, vestibulum vel orci. Etiam suscipit vehicula risus."
       }
     ]
   };
