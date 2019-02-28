147d784967bf23b466ba11892c1fd29a86983181
Miguel Machado <miiguellmp@gmail.com>
2019-02-28 14:12:55 -0300

    Criando e reutilizando um componente ja criado dentro de outro

--- Bootcamp/modulo01/src/index.js
+++ Bootcamp/modulo01/src/index.js
@@ -1,3 +1,21 @@
-const soma = (a, b) => a + b;
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
 
-alert(soma(7, 3));
+class Button extends Component {
+  render() {
+    return <a href="">Salvar</a>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Rocketseat</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
