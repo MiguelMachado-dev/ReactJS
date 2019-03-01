306b102325fb4422bfe952614bfef98ac80a2412
Miguel Machado <miiguellmp@gmail.com>
2019-03-01 10:52:22 -0300

    criando componente lista de repositorios

---
+++ Bootcamp/modulo02/gitcompare/src/pages/Main/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Container, Form } from './styles';
+
+import logo from '../../assets/logo.png';
+import CompareList from '../../components/CompareList';
+
+const Main = () => (
+  <Container>
+    <img src={logo} alt="Github Compare" />
+
+    <Form>
+      <input type="text" placeholder="usuário/repositório" />
+      <button type="submit">OK</button>
+    </Form>
+
+    <CompareList />
+  </Container>
+);
+
+export default Main;
