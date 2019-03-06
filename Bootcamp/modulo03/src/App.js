b25e2df6d7e0f545d82a4520b37eeef66bddb98d
Miguel Machado <miiguellmp@gmail.com>
2019-03-06 10:19:49 -0300

    lista de todos

--- Bootcamp/modulo03/src/App.js
+++ Bootcamp/modulo03/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
+import TodoList from './TodoList';
 
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World</h1>
+    <TodoList />
   </Provider>
 );
 
---
+++ Bootcamp/modulo03/src/store/reducers/todos.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = [{ id: 1, text: 'Fazer café' }, { id: 2, text: 'Estudar React' }];
+
+export default function todos(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
