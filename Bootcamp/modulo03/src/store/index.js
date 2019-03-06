a355513bd9166469f88c4cf1806ca7e25ff6d195
Miguel Machado <miiguellmp@gmail.com>
2019-03-06 14:47:21 -0300

    reactotron config

--- Bootcamp/modulo03/src/store/index.js
+++ Bootcamp/modulo03/src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const tronMiddleware = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer : () => {};
+const store = createStore(reducers, compose(tronMiddleware()));
 
 export default store;
