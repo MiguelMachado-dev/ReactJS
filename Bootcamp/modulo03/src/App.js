7eb08c55c942320db593c7d4aa3772e144e7acfb
Miguel Machado <miiguellmp@gmail.com>
2019-03-06 14:50:45 -0300

    new app

--- Bootcamp/modulo03/src/App.js
+++ Bootcamp/modulo03/src/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import './config/reactotron';
 import store from './store';
-import TodoList from './TodoList';
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <h1>Hello World</h1>
   </Provider>
 );
 
--- Bootcamp/modulo03/src/store/actions/todos.js
+++
@@ -1,9 +0,0 @@
-export const addTodo = text => ({
-  type: 'ADD_TODO',
-  payload: { text },
-});
-
-export const removeTodo = id => ({
-  type: 'REMOVE_TODO',
-  payload: { id },
-});
