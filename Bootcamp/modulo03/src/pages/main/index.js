753bd790c7a7f6b04776fc2d9480f7b7afc2db79
Miguel Machado <miiguellmp@gmail.com>
2019-03-06 15:27:20 -0300

    interface da app

--- Bootcamp/modulo03/src/pages/main/index.js
+++ Bootcamp/modulo03/src/pages/main/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    const { repositoryInput } = this.setState;
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={e => ({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong>
+              (blah blah blah description)
+            </p>
+            <a href="http://github.com/facebook/react">Acessar</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
