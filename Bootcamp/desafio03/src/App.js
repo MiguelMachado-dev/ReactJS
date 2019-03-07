e7a985242d907d1f6b3643009ced6d08071f1ae8
Miguel Machado <miiguellmp@gmail.com>
2019-03-07 16:13:16 -0300

    estilos global

--- Bootcamp/desafio03/src/App.js
+++ Bootcamp/desafio03/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import MapGL, { Marker } from 'react-map-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import dotenv from 'dotenv';
+import GlobalStyle from './styles/index';
 
 dotenv.config();
 
@@ -12,8 +13,8 @@ export default class Map extends Component {
     viewport: {
       width: window.innerWidth,
       height: window.innerHeight,
-      latitude: -23.5439948,
-      longitude: -46.6065452,
+      latitude: -22.290869,
+      longitude: -42.535898,
       zoom: 14,
     },
   };
@@ -28,47 +29,55 @@ export default class Map extends Component {
   }
 
   resize = () => {
+    const { viewport } = this.state;
     this.setState({
       viewport: {
-        ...this.state.viewport,
+        ...viewport,
         width: window.innerWidth,
         height: window.innerHeight,
       },
     });
   };
 
+  // eslint-disable-next-line class-methods-use-this
   handleMapClick(e) {
     const [latitude, longitude] = e.lngLat;
 
+    // eslint-disable-next-line no-alert
     alert(`Latitude: ${latitude} \nLongitude: ${longitude}`);
   }
 
   render() {
+    const { viewport } = this.state;
     return (
-      <MapGL
-        {...this.state.viewport}
-        onClick={this.handleMapClick}
-        mapStyle="mapbox://styles/mapbox/basic-v9"
-        mapboxApiAccessToken="pk.eyJ1IjoibW1jb2RpbmciLCJhIjoiY2pzeXlheGV1MDAxYTN5bGUybjdnc3F0cyJ9.1YhtjrA5gSzNeu18rKXbgA"
-        onViewportChange={viewport => this.setState({ viewport })}
-      >
-        <Marker
-          latitude={-23.5439948}
-          longitude={-46.6065452}
+      <Fragment>
+        <GlobalStyle />
+        <MapGL
+          {...viewport}
           onClick={this.handleMapClick}
-          captureClick
+          mapStyle="mapbox://styles/mapbox/basic-v9"
+          mapboxApiAccessToken="pk.eyJ1IjoibW1jb2RpbmciLCJhIjoiY2pzeXlheGV1MDAxYTN5bGUybjdnc3F0cyJ9.1YhtjrA5gSzNeu18rKXbgA"
+          // eslint-disable-next-line no-shadow
+          onViewportChange={viewport => this.setState({ viewport })}
         >
-          <img
-            style={{
-              borderRadius: 100,
-              width: 48,
-              height: 48,
-            }}
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt="User"
-          />
-        </Marker>
-      </MapGL>
+          <Marker
+            latitude={-22.290869}
+            longitude={-42.535898}
+            onClick={this.handleMapClick}
+            captureClick
+          >
+            <img
+              style={{
+                borderRadius: 100,
+                width: 48,
+                height: 48,
+              }}
+              src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+              alt="User"
+            />
+          </Marker>
+        </MapGL>
+      </Fragment>
     );
   }
 }
