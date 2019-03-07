040911f00723ba5f18af50426e46c63b2c33cf39
Miguel Machado <miiguellmp@gmail.com>
2019-03-07 15:38:52 -0300

    mapa configurado

--- Bootcamp/desafio03/src/App.js
+++ Bootcamp/desafio03/src/App.js
@@ -1,5 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MapGL, { Marker } from 'react-map-gl';
 
-const App = () => <h1>Hello World</h1>;
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-export default App;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default class Map extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -23.5439948,
+      longitude: -46.6065452,
+      zoom: 14,
+    },
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize);
+    this.resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+    });
+  };
+
+  handleMapClick(e) {
+    const [latitude, longitude] = e.lngLat;
+
+    alert(`Latitude: ${latitude} \nLongitude: ${longitude}`);
+  }
+
+  render() {
+    return (
+      <MapGL
+        {...this.state.viewport}
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoibW1jb2RpbmciLCJhIjoiY2pzeXlheGV1MDAxYTN5bGUybjdnc3F0cyJ9.1YhtjrA5gSzNeu18rKXbgA"
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        <Marker
+          latitude={-23.5439948}
+          longitude={-46.6065452}
+          onClick={this.handleMapClick}
+          captureClick
+        >
+          <img
+            style={{
+              borderRadius: 100,
+              width: 48,
+              height: 48,
+            }}
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt="User"
+          />
+        </Marker>
+      </MapGL>
+    );
+  }
+}
