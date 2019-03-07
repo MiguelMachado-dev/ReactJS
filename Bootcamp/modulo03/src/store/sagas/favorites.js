65805768faeddd5822473917beb4b1619a713311
Miguel Machado <miiguellmp@gmail.com>
2019-03-07 14:42:46 -0300

    repositorios duplicado

--- Bootcamp/modulo03/src/store/sagas/favorites.js
+++ Bootcamp/modulo03/src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Creators as FavoriteActions } from '../ducks/favorites';
@@ -7,14 +7,20 @@ export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
 
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url,
-    };
+    const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
 
-    yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoriteFailure('Repositório duplicado'));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url,
+      };
+
+      yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+    }
   } catch (err) {
     yield put(FavoriteActions.addFavoriteFailure('Erro ao adicionar repositório'));
   }
