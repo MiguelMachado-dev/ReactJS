80178fdb1c22afc4f75c115aa2450dc25d7971f9
Miguel Machado <miiguellmp@gmail.com>
2019-09-23 10:47:37 -0300

    fetching content

--- complete-react-Andrei-Course/monsters-rolodex/src/App.js
+++ complete-react-Andrei-Course/monsters-rolodex/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,19 +6,21 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello, Miguel!"
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Hello, World!" })}>
-            Change Text
-          </button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
