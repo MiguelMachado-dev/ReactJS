a1477e104ea3a78d72a7562a2bed86ef6904ee0d
Miguel Machado <miiguellmp@gmail.com>
2019-09-24 12:00:36 -0300

    eslinting and destructuring props

--- complete-react-Andrei-Course/monsters-rolodex/src/components/card/card.component.jsx
+++ complete-react-Andrei-Course/monsters-rolodex/src/components/card/card.component.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import "./card.styles.css";
+import './card.styles.css';
 
-export const Card = props => (
-  <div className="card-container">
-    <img
-      alt="Monster"
-      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
-    />
-    <h2> {props.monster.name} </h2>
-    <p> {props.monster.email} </p>
-  </div>
-);
+export const Card = ({ name, email, id }) => {
+  return (
+    <div className="card-container">
+      <img
+        alt="Monster"
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2> {name} </h2>
+      <p> {email} </p>
+    </div>
+  );
+};
