28ffcbb1eb52a8782d25f132b3cb115d137f1153
Miguel Machado <miiguellmp@gmail.com>
2019-09-24 12:00:16 -0300

    prettier

--- complete-react-Andrei-Course/monsters-rolodex/src/App.js
+++ complete-react-Andrei-Course/monsters-rolodex/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component";
-import "./App.css";
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
+
   render() {
     return (
       <div className="App">
