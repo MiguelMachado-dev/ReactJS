bc27623df904abf6c2fdb10cc5e1ab16ed7d577f
Miguel Machado <miiguellmp@gmail.com>
2019-12-12 17:31:47 -0200

    handling events

--- complete-react-Andrei-Course/monsters-rolodex/src/App.js
+++ complete-react-Andrei-Course/monsters-rolodex/src/App.js
@@ -11,12 +11,18 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
   }
 
   render() {
@@ -27,9 +33,10 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
