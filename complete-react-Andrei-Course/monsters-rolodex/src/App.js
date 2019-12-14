add7b48d84430827fbd793154ee1066a2fe7a209
Miguel Machado <miiguellmp@gmail.com>
2019-12-14 10:42:47 -0300

    props in constructor

--- complete-react-Andrei-Course/monsters-rolodex/src/App.js
+++ complete-react-Andrei-Course/monsters-rolodex/src/App.js
@@ -4,8 +4,8 @@ import { SearchBox } from './components/SearchBox';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       monsters: [],
