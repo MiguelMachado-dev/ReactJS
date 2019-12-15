784b3055dae479b304cedc28ade895b43a9c4ab3
Miguel Machado <miiguellmp@gmail.com>
2019-12-15 15:01:59 -0300

    using absolute paths

--- complete-react-Andrei-Course/ecommerce-project/src/pages/homepage/homepage.component.jsx
+++ complete-react-Andrei-Course/ecommerce-project/src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Directory from '../../components/directory/directory.components';
+import Directory from '~/components/directory/directory.components';
 
 import './homepage.styles.scss';
 
