713d1b6496ff0d88e5a2f8f2183fdaa0d11c10be
Miguel Machado <miiguellmp@gmail.com>
2019-12-19 21:33:49 -0300

    adding header component to app.js

--- complete-react-Andrei-Course/ecommerce-project/src/App.js
+++ complete-react-Andrei-Course/ecommerce-project/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from 'components/header/header.component';
 import ShopPage from 'pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component';
 
@@ -9,6 +10,7 @@ import 'App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
